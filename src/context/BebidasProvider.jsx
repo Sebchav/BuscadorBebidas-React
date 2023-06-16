c980ae29c40e0a1f950cf2ada75191b33ec97e0d
Chava <schavarria13@alumnos.uaq.mx>
2023-06-16 13:55:13 -0600

    Project 23 finished

--- src/context/BebidasProvider.jsx
+++ src/context/BebidasProvider.jsx
@@ -5,6 +5,30 @@ const BebidasContext = createContext();
 
 const BebidasProvider = ({children})=>{
    const [ bebidas, setBebidas ] = useState([]);
+   const [ modal, setModal ] = useState(false);
+   const [ bebidaId, setBebidaId ] = useState(null)
+   const [ receta, setReceta ] = useState({})
+   const [ cargando, setCargando ] = useState(false)
+
+   useEffect(()=>{
+    setCargando(true);
+    const obtenerReceta = async() =>{
+        if(!bebidaId) return;
+
+        try{
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${bebidaId}`;
+
+            const {data} = await axios(url);
+
+            setReceta(data.drinks[0])
+        }catch(error){
+            console.log(error)
+        } finally {
+            setCargando(false);
+        }
+    }
+    obtenerReceta();
+   }, [bebidaId])
 
    const consultarBebida = async datos => {
         try{
@@ -18,11 +42,24 @@ const BebidasProvider = ({children})=>{
         }
    }
 
+   const handleModalClick = ()=>{
+    setModal(!modal);
+   }
+
+   const handleBebidaIdClick = id => {
+    setBebidaId(id);
+   }
+
     return (
         <BebidasContext.Provider
             value={{
                consultarBebida,
-               bebidas
+               bebidas,
+               handleModalClick,
+               modal,
+               handleBebidaIdClick,
+               receta,
+               cargando
             }}
         >
             {children}
